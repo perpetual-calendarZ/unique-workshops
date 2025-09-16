df0f60f34085ca8aa51d561a49d8ce8bf981e9fe
Zeitgeistsignet <phygitaloffice@proton.me>
2025-09-16 20:07:06 -0300

    new correct
    Collection new

--- 1-create_collection.js
+++ 1-create_collection.js
@@ -1,41 +1,45 @@
-// 1-create-collection.js
-// A MUDANÇA É NESTA LINHA: removemos o '/keyring' do final
-const { KeyringProvider } = require('@unique-nft/accounts'); 
-const { Client } = require('@unique-nft/sdk');
+const { KeyringProvider } = require('@unique-nft/accounts/keyring');
+const { Sdk } = require('@unique-nft/sdk');
 const { mnemonic } = require('./config');
 
 async function main() {
   const signer = await KeyringProvider.fromMnemonic(mnemonic);
-  
-  const client = new Client({
-    baseUrl: 'https://rest.unique.network/opal/v1',
+  const address = signer.getAddress();
+
+  const sdk = new Sdk({
+    // A versão antiga usa o endereço wss://
+    baseUrl: 'wss://ws-opal.unique.network',
     signer,
   });
-  const sdk = client.sdk;
 
   const collectionData = {
     name: 'perpetual calendar traveler',
     description: 'A collection of perpetual calendar traveler NFTs',
     tokenPrefix: 'PCT',
+    // O schema também é um pouco diferente nesta versão
     schema: {
       schemaName: 'unique',
       schemaVersion: '1.0.0',
-      coverPicture: { url: 'https://exemplo.com/capa-colecao.png' },
-      image: { urlTemplate: '{id}' },
-      attributesSchema: {
-        '0': { name: { _: 'Cor de Fundo' }, type: 'string' },
-        '1': { name: { _: 'Acessório' }, type: 'string' },
-        '2': { name: { _: 'Nível' }, type: 'number' },
+      coverPicture: {
+        url: 'https://exemplo.com/capa-colecao.png',
       },
-    },
+      image: {
+        urlTemplate: '{id}',
+      },
+      attributesSchemaVersion: '1.0.0',
+      attributesSchema: {
+        0: { name: { _: 'Cor de Fundo' }, type: 'string' },
+        1: { name: { _: 'Acessório' }, type: 'string' },
+        2: { name: { _: 'Nível' }, type: 'number' },
+      }
+    }
   };
 
   console.log('Criando a coleção "perpetual calendar traveler"...');
-  
-  const { address } = signer.getAccount();
-  const { collectionId } = await sdk.collections.creation.submitWaitResult({
-    ...collectionData,
+
+  const { collectionId } = await sdk.collections.create.submitWaitResult({
     address,
+    ...collectionData,
   });
 
   console.log('SUCESSO! Coleção criada!');
